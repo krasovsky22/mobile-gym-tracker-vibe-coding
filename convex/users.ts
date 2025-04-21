b323463a261d69e8c3def026fdaa1bac99e36f21
Vlad Krasovsky <krasovsky@kentik.com>
2025-04-20 20:03:40 -0700

    correct way to get user

--- convex/users.ts
+++ convex/users.ts
@@ -1,5 +1,6 @@
 import { Id } from './_generated/dataModel';
 import { mutation } from './_generated/server';
+import { getAuthUserId } from "@convex-dev/auth/server";
 
 export const ensureUser = mutation({
   handler: async (ctx) => {
@@ -8,11 +9,13 @@ export const ensureUser = mutation({
       throw new Error('Not authenticated');
     }
 
-    const userId = identity.subject.split('|')[0] as Id<'users'>;
-    const user = await ctx.db
-      .query('users')
-      .withIndex('by_id', (q) => q.eq('_id', userId))
-      .first();
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error('Not authenticated');
+    }
+
+    const user = await ctx.db.get(userId);
 
     if (!user) {
       throw new Error('User not found.');
